0acab1a2f1f0807cb2d8f19e75c1359aece018fd
Johann Kirchkesner <yinzet@web.de>
2016-12-18 22:15:14 +0100

    fix error in manifest

--- hce/src/main/java/de/jokir/hceexample/MyHostApduService.java
+++ hce/src/main/java/de/jokir/hceexample/MyHostApduService.java
@@ -33,23 +33,6 @@ public class MyHostApduService extends HostApduService {
         Log.d(TAG, "onCreate");
     }
 
-    /**
-     * This method will be called when a command APDU has been received from a remote device. A
-     * response APDU can be provided directly by returning a byte-array in this method. In general
-     * response APDUs must be sent as quickly as possible, given the fact that the user is likely
-     * holding his device over an NFC reader when this method is called.
-     * If there are multiple services that have registered for the same AIDs in
-     * their meta-data entry, you will only get called if the user has explicitly selected your
-     * service, either as a default or just for the next tap.
-     * This method is running on the main thread of your application. If you
-     * cannot return a response APDU immediately, return null and use the {@link
-     * #sendResponseApdu(byte[])} method later.
-     *
-     * @param apdu   The APDU that received from the remote device
-     * @param extras A bundle containing extra data. May be null.
-     * @return a byte-array containing the response APDU, or null if no response APDU can be sent
-     * at this point.
-     */
     @Override
     public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
         Log.d(TAG, "processCommandApdu: " + new String(apdu));
@@ -57,20 +40,13 @@ public class MyHostApduService extends HostApduService {
 
         if (Arrays.equals(apdu, SELECT_APDU)) {
             Log.d(TAG, "Application selected");
-            return ByteUtils.stringToAsciiByteArray("Hello Desktop!");
+            return "hello reader".getBytes();
         } else {
             Log.d(TAG, "Received: " + new String(apdu));
-            return ByteUtils.stringToAsciiByteArray("Message from android: " + counter++);
+            return ("message from hce: " + counter++).getBytes();
         }
     }
 
-    /**
-     * Called if the connection to the NFC card is lost, in order to let the application know the
-     * cause for the disconnection (either a lost link, or another AID being selected by the
-     * reader).
-     *
-     * @param reason Either DEACTIVATION_LINK_LOSS or DEACTIVATION_DESELECTED
-     */
     @Override
     public void onDeactivated(int reason) {
 
